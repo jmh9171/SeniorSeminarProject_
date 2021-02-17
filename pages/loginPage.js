c6ac99474cc7b1da7a97a5a5cc8e9e7973babf5a
Louis Fries <theguardiian13@gmail.com>
2021-02-17 17:26:12 -0500

    Update loginPage.js

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -13,7 +13,17 @@ export default function hello() {
             <Header/>
             <h1>
                 This is the Login Page
+
             </h1>
+            <h2>
+              <form action="/action_page.php">
+              <label htmlFor="fname">First name:</label><br />
+              <input type="text" id="fname" name="fname" defaultValue="John" /><br />
+              <label htmlFor="lname">Last name:</label><br />
+              <input type="text" id="lname" name="lname" defaultValue="Doe" /><br /><br />
+              <input type="submit" defaultValue="Submit" />
+              </form>
+            </h2>
         
             <style jsx global>{`
                 html,
