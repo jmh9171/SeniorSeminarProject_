cad7814339f40c93ed8787b5bee3e6f2198b7f61
Louis Fries <theguardiian13@gmail.com>
2021-02-21 12:57:25 -0500

    About Page
    Did an about page

--- pages/about.js
+++ pages/about.js
@@ -6,13 +6,101 @@ export default function about() {
 return(
     <div>
     <Head>
-        <title>This is the login page</title>
+        <title>About Us</title>
         <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Header/>
-    <h1>
-        This is the About Page
-    </h1>
+
+    </Head> 
+ <Header/> 
+
+ <style jsx>{`
+
+    body, html {
+      height: 100%;
+      font-family: "Inconsolata", sans-serif;
+    }
+    
+    .bgimg {
+      background-position: center;
+      background-size: cover;
+      background-image: url("/images/AboutHead.jpg");
+      min-height: 75%;
+    }
+    
+    .menu {
+      display: none;
+    }
+
+            `}</style>
+
+    <div>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata" />
+
+        <div className="w3-container">
+        <center><img src="images/AboutHead.jpg" style={{width: '100%', maxHeight: '850px'}} className="w3-margin-top" /></center>
+        <div className="w3-display-middle w3-center">
+          <span className="w3-text-white w3-tag w3-wide" style={{fontSize: '100px'}}>Playgroup<br /></span>
+        </div>
+        <div className="w3-display-bottomleft w3-center w3-padding-large">
+          <span className="w3-text-white">Img src: pexels.com</span>
+        </div>
+        </div>
+      
+        
+        {/* Add a background color and large text to the whole page */}
+
+        <div className="w3-sand w3-grayscale w3-large">
+          {/* About Container */}
+          <div className="w3-container" id="about">
+            <div className="w3-content" style={{maxWidth: '700px'}}>
+              <h5 className="w3-center w3-padding-64"><span className="w3-tag w3-wide">ABOUT PLAYGROUP</span></h5>
+              <p>Playgroup is designed to connect small groups of individuals through online interaction easily to help alleviate the pain of using game specific group finders.  No matter what the game is, users will easily be able to find other like-minded individuals to play their favorite games with. </p>
+              <p>This web application is designed to collect as little information as possible from its users and will not be providing any user data to other large corporations.  Our mission is to create a safe, non-invasive, free platform for people to find or create their own niche groups on.</p>
+              <div className="w3-panel w3-leftbar w3-light-grey">
+                <p><i>"Privacy is not an option, and it shouldn't be the price we accept for just getting on the Internet."</i></p>
+                <p>Chief Executive Officer of AVG Technologies: Gary Kovacs</p>
+              </div>
+            </div>
+          </div>
+          
+          {/* Menu Container */}
+          <div className="w3-container" id="menu">
+            <div className="w3-content" style={{maxWidth: '750px'}}>
+              <h5 className="w3-center w3-padding-48"><span className="w3-tag w3-wide">THE MISSION</span></h5>
+
+             
+            <div className="w3-container" id="about">
+
+            <div>
+              <center><img src="/images/Heart.png" style={{width: '50%', maxWidth: '1000px'}} className="w3-margin-top" /></center>
+              <center><h3><p>Connect with others who have a similar approah to gaming as you!</p><br /></h3></center>
+            </div>
+
+
+            <div>
+              <center><img src="/images/Profile.png" style={{width: '50%', maxWidth: '1000px'}} className="w3-margin-top" /></center>
+              <center><h3><p>Customize your profile and Playgroup in a way that works for you.</p><br /></h3></center>
+            </div>
+
+
+
+            </div>
+             
+
+
+
+
+            </div>
+          </div>
+          {/* End page content */}
+        </div>
+      </div>
+
+
+   
+
     <style jsx global>{`
         html,
         body {
