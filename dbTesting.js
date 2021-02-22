462da9ca92655f848c555b3faf5ee4a8b231c106
Jacob <jacobheylek@gmail.com>
2021-02-22 11:26:10 -0500

    dbTesting

--- dbTesting.js
+++ dbTesting.js
@@ -27,23 +27,57 @@ const mysql = require('mysql');
 
 // First you need to create a connection to the database
 // Be sure to replace 'user' and 'password' with the correct values
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'jmh9171',
-    password: 'temp!9171',
-    database: 'jmh9171'
-});
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'jmh9171',
+//     password: 'temp!9171',
+//     database: 'jmh9171'
+// });
 
-con.connect((err) => {
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
+// con.connect((err) => {
+//   if(err){
+//     console.log('Error connecting to Db');
+//     return;
+//   }
+//   console.log('Connection established');
+// });
 
-con.end((err) => {
-  // The connection is terminated gracefully
-  // Ensures all remaining queries are executed
-  // Then sends a quit packet to the MySQL server.
-});
+// con.end((err) => {
+//   // The connection is terminated gracefully
+//   // Ensures all remaining queries are executed
+//   // Then sends a quit packet to the MySQL server.
+// });
+
+module.exports.createConnection = require('mysql');
+    const con = mysql.createConnection({
+           host: 'localhost',
+           user: 'jmh9171',
+           password: 'temp!9171',
+           database: 'jmh9171'
+      });
+    connection.connect((err) => {
+      if (err) throw err;
+      console.log('Connected!');
+      return con;
+    });
+
+
+
+// const connection = new Promise((resolve, reject) => {
+//   var conn = mysql.createConnection({
+//     host: 'penguin',
+//     user: 'jmh9171',
+//     password: 'temp!9171',
+//     database: 'jmh9171'
+//   });
+//   conn.connect((err) => {
+//     if (err) reject();
+//     console.log('Connected!');
+//     resolve(conn);
+//   });
+// })
+// connection.end((err) => {
+//   // The connection is terminated gracefully
+//   // Ensures all remaining queries are executed
+//   // Then sends a quit packet to the MySQL server.
+// });
