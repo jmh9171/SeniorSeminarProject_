3f447ea782f4497a359c674ae9bcc24bb261dd0f
Louis Fries <theguardiian13@gmail.com>
2021-03-19 17:04:24 -0400

    Fixed event props
    Changed the props

--- components/event/index.jsx
+++ components/event/index.jsx
@@ -14,31 +14,32 @@ function Testevent() {
 
 function Eventname(props){
     return (
-      <b><p>{props.eventname}:</p></b>
+      <h2 className="w3-center"><b><p>{props.eventname}:</p></b></h2>
+      
     );
   }
 
   function Playgroupname2(props){
     return (
-      <p>{props.playgroupname2}</p>
+      <p><b>Playgroup:</b><p>{props.playgroupname2}</p></p>
     );
   }
 
   function Date(props){
     return (
-      <p>{props.date}</p>
+      <p><b>Date:</b><p>{props.date}</p></p>
     );
   }
 
   function Time(props){
     return (
-      <p>{props.time}</p>
+      <p><b>Time:</b><p>{props.time}</p></p>
     );
   }
 
   function Eventdescription(props){
     return (
-      <p>{props.eventdescription}</p>
+      <p><b>Description:</b><p>{props.eventdescription}</p></p>
     );
   }
 
@@ -49,11 +50,11 @@ const Event = () => (
   <center>
   <div align="left"style={{maxWidth: '400px', maxHeight: '300px', overflow: 'hidden', overflow: 'auto'}}>
                 <p>
-                      <h2 className="w3-center"><Eventname eventname="1v1 Tournament"/></h2>
-                      <b>Playgroup:</b><Playgroupname2 playgroupname2="Super cool MC Playgroup #2"/>
-                      <b>Date:</b><Date date="3/19/21" />
-                      <b>Time:</b><Time time="3:30"/>
-                      <b>Description:</b><Eventdescription eventdescription="We will play Minecraft for 2 hours" />      
+                      <Eventname eventname="1v1 Tournament"/>
+                      <Playgroupname2 playgroupname2="Super cool MC Playgroup #2"/>
+                      <Date date="3/19/21" />
+                      <Time time="3:30"/>
+                      <Eventdescription eventdescription="We will play Minecraft for 2 hours" />      
                </p>
     </div>
 
