4eeae004fc32b31a8de9220fedae8b253ab8b6c1
Louis Fries <theguardiian13@gmail.com>
2021-03-31 10:45:07 -0400

    Update index.js

--- components/creategroup/index.jsx
+++ components/creategroup/index.jsx
@@ -8,6 +8,7 @@ const Creategroupform = () => (
             <label htmlFor="gname">Group Name</label>
             <input type="text" id="gname" name="groupname" placeholder="Group name.." />
             
+            
 {/*             
             <label htmlFor="tag1">Tag #1</label>
             <select id="tag1" name="tag1">
@@ -35,7 +36,7 @@ const Creategroupform = () => (
               <option value="competitive">COMPETITIVE</option>
               <option value="racing">RACING</option>
             </select> 
-*/}
+ */}
 
 
 
