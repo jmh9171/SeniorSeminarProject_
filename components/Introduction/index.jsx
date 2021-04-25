64baa5de6ce0431342529902037d09a58f17dbe7
Louis Fries <theguardiian13@gmail.com>
2021-04-24 23:36:52 -0400

    Changes profile
    changes profile

--- components/Introduction/index.jsx
+++ components/Introduction/index.jsx
@@ -10,7 +10,7 @@ function Aboutme(props) {
 const Introduction = ({ description }) => (
   <introduction>
     <div className="w3-container w3-card w3-white w3-margin-bottom">
-      <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-cube fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />Introduction</h2>
+      <h2 className="w3-text-grey w3-padding-16"><i className="fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />Introduction</h2>
       <div className="w3-container">
         <h5 className="w3-opacity"><b>About me</b></h5>
         <Aboutme aboutme={description} />
--- components/profilePlaygroups/index.jsx
+++ components/profilePlaygroups/index.jsx
@@ -16,7 +16,7 @@ const Profileplaygroups = () => (
     <profileplaygroups>
   
   <div className="w3-container w3-card w3-white">
-              <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-list fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />My Playgroups</h2>
+              <h2 className="w3-text-grey w3-padding-16"><i className="fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />My Playgroups</h2>
               <div className="w3-container">
                 
                 <Gametitle gametitle="Minecraft"/>
