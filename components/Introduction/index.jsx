20fb0a55f135f9f6fa956cdb69ac7dc21618547e
Louis Fries <theguardiian13@gmail.com>
2021-04-24 23:34:17 -0400

    Updates
    Update

--- components/Introduction/index.jsx
+++ components/Introduction/index.jsx
@@ -64,7 +64,7 @@ const Introduction = () => (
     <introduction>
     
         <div className="w3-container w3-card w3-white w3-margin-bottom">
-              <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-cube fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />Introduction</h2>
+              <h2 className="w3-text-grey w3-padding-16"><i className="fa-fw w3-margin-right w3-xxlarge w3-text-dark-grey" />Introduction</h2>
 
               <div className="w3-container">
                 <h5 className="w3-opacity"><b>About me</b></h5>
