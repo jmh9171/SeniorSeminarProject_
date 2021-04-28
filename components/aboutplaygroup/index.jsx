b2a17685cb6ff164156b63f9e074952982769798
Louis Fries <theguardiian13@gmail.com>
2021-04-28 14:36:02 -0400

    Style changes for about page
    Changed style for about page

--- components/aboutplaygroup/index.jsx
+++ components/aboutplaygroup/index.jsx
@@ -3,16 +3,21 @@ const Aboutplaygroup = () => (
   
 
         <div className="w3-container" id="about">
-            <div className="w3-content" style={{maxWidth: '700px'}}>
-              <h4 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide"><b>ABOUT PLAYGROUP</b></span></h4>
+            <div className="w3-content" style={{maxWidth: '900px'}}>
+              <h4 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide" style={{fontSize: "30px"}}><b>ABOUT PLAYGROUP</b></span></h4>
               <p>Playgroup is designed to connect small groups of individuals through online interaction easily to help alleviate the pain of using game specific group finders.  No matter what the game is, users will easily be able to find other like-minded individuals to play their favorite games with. </p>
+              <p>Other group finders can be shady and collect information about you that you wouldn't want getting out to the public.  Depending on the application, they may also use this data to track you across other applications made by other companies.  They do this by selling your data!</p>
               <p>This web application is designed to collect as little information as possible from its users and will not be providing any user data to other large corporations.  Our mission is to create a safe, non-invasive, free platform for people to find or create their own niche groups on.</p>
+              <p>Our team has made design conscious decisions to reflect this such as avoiding using database services that automatically collect and analyze user data.  The only sata that we store is the data that is necessry to run the web application.</p>
               <div className="w3-panel w3-leftbar w3-light-grey">
                 <p><i>"Privacy is not an option, and it shouldn't be the price we accept for just getting on the Internet."</i></p>
                 <p>Chief Executive Officer of AVG Technologies: Gary Kovacs</p>
               </div>
             </div>
           </div>
+
+         
+          
 
 
     </aboutplaygroup>
--- components/themission/index.jsx
+++ components/themission/index.jsx
@@ -2,20 +2,20 @@ const Themission = () => (
     <themission>
   
             <div className="w3-container">
-                    <div className="w3-content" style={{maxWidth: '1000px'}}>
-                    <h5 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide"><b>THE MISSION</b></span></h5>
+                    <div className="w3-content" style={{maxWidth: '1100px'}}>
+                    <h5 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide" style={{fontSize: "30px"}}><b>THE MISSION</b></span></h5>
 
                     
                     <div className="w3-container" id="about">
 
                         <div>
                             <center><img src="/images/Heart.png" style={{width: '50%', maxWidth: '500px'}} className="w3-margin-top" /></center>
-                            <center><h3><p>Connect with others who have a similar approah to gaming as you!</p><br /></h3></center>
+                            <center><h3><p style={{fontSize: "30px"}}><b>Connect with others who have a similar approah to gaming as you!</b></p><br /></h3></center>
                         </div>
 
                         <div>
                             <center><img src="/images/Profile.png" style={{width: '50%', maxWidth: '1000px'}} className="w3-margin-top" /></center>
-                            <center><h3><p>Customize your profile and Playgroup in a way that works for you.</p><br /></h3></center>
+                            <center><h3><p style={{fontSize: "30px"}}><b>Customize your profile and Playgroup in a way that works for you.</b></p><br /></h3></center>
                         </div>
                     </div>
                     </div>
