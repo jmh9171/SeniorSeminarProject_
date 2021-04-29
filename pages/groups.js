d4f311d3cae21fa8577b890e5cd6fe0bd8f4ae47
Louis Fries <theguardiian13@gmail.com>
2021-04-29 14:57:05 -0400

    Added bg to scroll bar
    Added bg to the scrolling sections of groups and findgroups

--- pages/groups.js
+++ pages/groups.js
@@ -22,7 +22,7 @@ return(
           <span className="w3-text-white" style={{fontSize: '90px'}}>My Groups<br /></span>
           <center> 
             
-  <div className="w3-middle-align" style={{maxWidth: '800px', maxHeight: '250px', overflow: 'hidden', overflow: 'auto'}}>
+  <div className="w3-middle-align" style={{maxWidth: '800px', maxHeight: '250px', overflow: 'hidden', overflow: 'auto', background: "rgba(46, 49, 49, 0.75)"}}>
 
         
 
