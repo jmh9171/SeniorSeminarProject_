b36caefbf95752e8f7198aca0d3e56945ba9d7ca
Louis Fries <theguardiian13@gmail.com>
2021-04-30 21:59:53 -0400

    Changed spacing
    spacing change

--- components/member/index.jsx
+++ components/member/index.jsx
@@ -57,16 +57,15 @@ function Aboutme(props){
 const Member = () => (
     <member>
 
-{/* Change to 0 for 3 in a row */}
     <div className="w3-third" style={{margin: "0px", padding: "10px"}}>
         <div className="w3-white w3-text-grey w3-card-4" style={{padding: "10px"}}>
             <center><p style={{padding: "0px"}}><i/><Username name="SoraSoren"/></p></center>
             <div className="w3-display-container w3-topmiddle" style={{padding: "5px", width: "95%"}}>
                   <Avatar avatarURL="https://placekitten.com/g/64/64"/>
-
+                  <p></p>
                         {/* Intro */}
                         <div className="w3-container w3-card w3-white w3-margin-bottom "  style={{}}>
-                            <h2 className="w3-text-grey w3-padding-10"><i className="fa-fw w3-margin-right w3-text-dark-grey" />Introduction</h2>
+                            <h2 className="w3-text-grey"><i className="fa-fw w3-margin-right w3-text-dark-grey" />Introduction</h2>
                             <div className="w3-container" >
                                 {/* Header */}
                                     <h5 className="w3-opacity"><b>About me</b></h5>
