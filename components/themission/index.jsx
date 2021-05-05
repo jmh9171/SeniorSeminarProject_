207ccce432ba2ee058d771d5e8e30a1d3bd0bf51
Louis Fries <theguardiian13@gmail.com>
2021-05-05 13:43:44 -0400

    final changes
    final changes

--- components/themission/index.jsx
+++ components/themission/index.jsx
@@ -1,26 +1,55 @@
 const Themission = () => (
     <themission>
   
-            {/* Menu Container */}
-            <div className="w3-container" id="menu">
-                    <div className="w3-content" style={{maxWidth: '1000px'}}>
-                    <h5 className="w3-center w3-padding-48"><span className="w3-tag w3-wide">THE MISSION</span></h5>
+            <div className="w3-container">
+                    <div className="w3-content" style={{maxWidth: '1100px'}}>
+                    <h5 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide" style={{fontSize: "30px"}}><b>THE MISSION</b></span></h5>
 
                     
                     <div className="w3-container" id="about">
 
                         <div>
                             <center><img src="/images/Heart.png" style={{width: '50%', maxWidth: '500px'}} className="w3-margin-top" /></center>
-                            <center><h3><p>Connect with others who have a similar approah to gaming as you!</p><br /></h3></center>
+                            <center><h3><p style={{fontSize: "30px"}}><b>Connect with others who have a similar approah to gaming as you!</b></p><br /></h3></center>
                         </div>
 
                         <div>
                             <center><img src="/images/Profile.png" style={{width: '50%', maxWidth: '1000px'}} className="w3-margin-top" /></center>
-                            <center><h3><p>Customize your profile and Playgroup in a way that works for you.</p><br /></h3></center>
+                            <center><h3><p style={{fontSize: "30px"}}><b>Customize your profile and Playgroup in a way that works for you.</b></p><br /></h3></center>
                         </div>
                     </div>
                     </div>
-                </div>
+
+        <table align="center">
+        <tbody>
+        <tr>
+            <th colSpan={3}> <h5 className="w3-center w3-padding-32"><span className="w3-tag w3-grey w3-wide" style={{fontSize: "30px"}}><b>TECHNOLOGIES USED</b></span></h5></th> 
+        </tr>
+        <tr>  
+            <td><p style={{fontSize: "25px"}}><b>Node.js - </b></p></td>
+            <td><p style={{fontSize: "25px"}}>Runtime environment</p></td>
+            <td><img src="/images/NodePic.jpg" style={{height: "110px", width: "180px"}} /></td>
+        </tr>
+        <tr>
+            <td><p style={{fontSize: "25px"}}><b>MySQL - </b></p></td>
+            <td><p style={{fontSize: "25px"}}>Database system</p></td>
+            <td><img src="/images/SQLPic.jpg" style={{height: "110px", width: "180px"}}/></td>
+        </tr>
+        <tr>
+            <td><p style={{fontSize: "25px"}}><b>React - </b></p></td>
+            <td><p style={{fontSize: "25px"}}>Front-end framework</p></td>
+            <td><img src="/images/ReactPic.jpg" style={{height: "110px", width: "180px"}}/></td>
+        </tr>
+        <tr>
+            <td><p style={{fontSize: "25px"}}><b>Next.js - </b></p></td>
+            <td><p style={{fontSize: "25px"}}>Back-end web framework</p></td>
+            <td><img src="/images/NextPic.jpg" style={{height: "110px", width: "180px"}}/></td>
+        </tr>
+        </tbody></table>
+
+
+
+            </div>
   
 
 
